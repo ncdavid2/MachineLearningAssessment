122c283b624070789a1f79645b8e792fa253a94f
ncdavid2 <ncdavid@web.de>
2024-11-11 19:11:02 +0000

    Fill Data

--- FIllColumns.py
+++ FIllColumns.py
@@ -5,10 +5,14 @@ finance_data = pd.read_csv(file_path)
 
 # Calculate the mean of the "Water Bill (£)" column, ignoring NaN values
 water_bill_avg = finance_data['Water Bill (£)'].mean()
+monthly_bill_avg = finance_data['Monthly Outing (£)'].mean()
 
 # Fill NaN values in "Water Bill (£)" with the calculated average
 finance_data['Water Bill (£)'] = finance_data['Water Bill (£)'].fillna(water_bill_avg)
 finance_data['Sky Sports (£)'] = finance_data['Sky Sports (£)'].fillna(0)
+finance_data['Other Expenses (£)'] = finance_data['Other Expenses (£)'].fillna(0)
+finance_data['Savings for Property (£)'] = finance_data['Savings for Property (£)'].fillna(0)
+finance_data['Monthly Outing (£)'] = finance_data['Monthly Outing (£)'].fillna(monthly_bill_avg)
 
 # Save the modified dataset to a new file
 output_file_path = 'personal_finance_employees_filled.csv'
